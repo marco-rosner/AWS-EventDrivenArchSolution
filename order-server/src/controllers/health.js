c2c0f5875e38419cfe855e01900d0ef04eea32d3
Marco Rosner <marco.rosner@gmail.com>
2024-04-03 19:04:35 -0300

    feat: Updating Health route to check MongoDB connection

--- order-server/src/controllers/health.js
+++ order-server/src/controllers/health.js
@@ -1,5 +1,11 @@
-function health(req, res) {
-    res.sendStatus(200)
+const mongoose = require('mongoose')
+
+function health(_, res) {
+    if (mongoose.connection.readyState === 1) {
+        res.sendStatus(200)
+    } else {
+        res.sendStatus(500)
+    }
 }
 
 module.exports = { health }
